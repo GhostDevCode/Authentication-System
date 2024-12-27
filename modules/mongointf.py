e72e700738cc4683c1d1204f6a2c05ce6897869c
GhostDevCode <ghostdevcode@gmail.com>
2024-12-27 19:34:50 +0100

    MongoDB Utility

--- modules/mongointf.py
+++ modules/mongointf.py
@@ -1,9 +1,14 @@
+'''
+This is a Python script for connecting to MongoDB.
+Configuration parameters can be found in a separate configuration file, located in the 'conf' directory.
+'''
+
+# Import the necessary libraries
 import pymongo
 import os
 
 class MongoInterface:
+    
     def main(self):
         return self
-    
-
     
